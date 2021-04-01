1929f60c64811da59db3e0adff0437fef3cb4005
Ken2399 <36779270+Ken2399@users.noreply.github.com>
2021-04-01 10:07:24 +0400

    Fixed static images (#2)
    Co-authored-by: Ken2399 <Ken@Iskander.local>

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,9 @@
 # pages/urls.py
 
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import ProductPageView, AboutPageView
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
-    path('', HomePageView.as_view(), name='home'),
+    path('product/', ProductPageView.as_view(), name='product'),
 ]
